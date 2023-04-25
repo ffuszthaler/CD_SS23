03f589d7c1e08dd41583a781d200be956484d1f4
ffuszthaler <flo.fuszthaler@gmail.com>
2023-04-25 12:48:17 +0200

    external files now work

--- final_assignment/Program.cs
+++ final_assignment/Program.cs
@@ -7,107 +7,62 @@ using static System.Environment;
 
 class FloodFill
 {
-  // static string[,] grid = new string[,]
-  // {
-  //       {"#", "#", "#", "#", "#", "#", "#", "#", "#", "#"},
-  //       {"#", ".", ".", ".", ".", ".", ".", ".", ".", "#"},
-  //       {"#", ".", "#", ".", "#", "#", "#", ".", ".", "#"},
-  //       {"#", ".", ".", ".", ".", ".", ".", ".", ".", "#"},
-  //       {"#", "#", "#", "#", "#", "#", "#", "#", "#", "#"},
-  // };
-
-  // static string[,] input = new string[,]
-  // {
-  //       {"#", "#", "#", "#", "#", "#", "#", "#", "#"},
-  //       {"#", ".", ".", ".", "#", ".", ".", ".", "#"},
-  //       {"#", ".", ".", ".", "#", ".", ".", ".", "#"},
-  //       {"#", ".", ".", "#", ".", ".", ".", ".", "#"},
-  //       {"#", "#", "#", ".", ".", ".", "#", "#", "#"},
-  //       {"#", ".", ".", ".", ".", "#", ".", ".", "#"},
-  //       {"#", ".", ".", ".", "#", ".", ".", ".", "#"},
-  //       {"#", ".", ".", ".", "#", ".", ".", ".", "#"},
-  //       {"#", "#", "#", "#", "#", "#", "#", "#", "#"},
-  // };
-
-
-
-  // private static int getColumnLength(string[][] jaggedArray, int columnIndex)
-  // {
-  //   int count = 0;
-  //   foreach (string[] row in jaggedArray)
-  //   {
-  //     if (columnIndex < row.Length) count++;
-  //   }
-  //   return count;
-  // }
-
-  static void Main()
+  static void Main(string[] args)
   {
-
-    // x: column - up, down
-    // y: row - left, right
-
+    // provide an input file with the layout
     string inputFile = File.ReadAllText("./input.txt");
-    // static string[,] inputTxt = inputTxtFile.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
-    // static string[][] inputTxt = inputFile.Split(new string[] { "", "], [", "]]" }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Split(',').ToArray()).ToArray();
 
-    // string[][] temp = inputFile.Split(',').Select(p => Regex.Split(p, "(?<=\\G.{2})")).ToArray();
-
-    // string[,] input = new String[temp.Length, temp[0].Length];
-    // string[,] input = inputFile.Split(new String[,] { { "\n" } }, StringSplitOptions.None);
-
-    // // convert string[][] to string[,]
-    // for (int i = 0; i != temp.Length; i++)
-    //   for (int j = 0; j != temp[0].Length; j++)
-    //     input[i, j] = temp[i][j];
-
-    // Split the content by line breaks to get each row
-
+    // extract each row into an array
     string[] rows = inputFile.Split(Environment.NewLine);
-    Console.WriteLine(rows);
 
-    // Loop through each row and split it by comma to get each column value
+    // create an appropriately sized array for the algorithm
+    string[,] input = new string[rows.Length, rows[0].Split(' ').Length];
+
+    // loop through each row and split it by spaces to get each column value
     for (int i = 0; i < rows.Length; i++)
     {
-      string[] columns = rows[i].Split(',');
+      string[] columns = rows[i].Split(' ');
 
-      // Loop through each column and assign its value to the corresponding array element
+      // loop through each column and assign its value to the corresponding array element
       for (int j = 0; j < columns.Length; j++)
       {
-        string[,] input = new string[columns.Length, rows.Length];
-
-        input[i, j] = columns[j].Trim(); // Trim any white spaces around the value
+        // trim any white spaces around the value
+        input[i, j] = columns[j].Trim();
       }
     }
 
+    // introduction
+    Console.WriteLine("Welcome to the Flood-Fill Algorithm!");
+    Console.WriteLine("------------------------------------");
+    Console.WriteLine("INPORTANT");
+    Console.WriteLine("To change the layout, modify input.txt");
+    Console.WriteLine("Coordinate values start at 0, not 1!");
+    Console.WriteLine("------------------------------------");
 
-    // Fill the region starting at (5, 1) with color "@"
-    // Fill(input, 4, 4, "@", ".");
-    // Fill(grid, 3, 1, "@", ".");
+    // require user input for coords
+    Console.Write("Provide an X coordinate: ");
+    int xCoords = Convert.ToInt32(Console.ReadLine());
 
-    // for (int x = 0; x < getColumnLength(inputTxt, 0); x++)
-    // {
-    //   for (int y = 0; y < getColumnLength(inputTxt, 1); y++)
-    //   {
-    //     Console.Write(inputTxt[x][y] + " ");
-    //   }
-    //   Console.WriteLine();
-    // }
+    Console.Write("Provide an y coordinate: ");
+    int yCoords = Convert.ToInt32(Console.ReadLine());
+
+    // execute algorithm with provided coords
+    Fill(input, xCoords, yCoords, "@", ".");
   }
 
   static void Fill(string[,] layout, int x, int y, string new_string, string old_string)
   {
-    // Check if (x, y) is within the layout and the current new_string is not equal to the target new_string
+    // check if (x, y) is within the layout and the current new_string is not equal to the target new_string
     if (x < 0 || x >= layout.GetLength(0) || y < 0 || y >= layout.GetLength(1) || layout[x, y] != old_string)
     {
       return;
     }
 
-    // Set the current cell to the target new_string
+    // set the current cell to the target new_string
     layout[x, y] = new_string;
     Print(layout);
 
-    // Fill the neighboring cells recursively
+    // fill the neighboring cells recursively
     Fill(layout, x + 1, y, new_string, old_string);
     Fill(layout, x - 1, y, new_string, old_string);
     Fill(layout, x, y + 1, new_string, old_string);
@@ -116,16 +71,34 @@ class FloodFill
 
   static void Print(string[,] layout)
   {
+    // wait for half a second and clear console to prepare next animation frame
     Thread.Sleep(500);
     Console.Clear();
+
+    // create a new output text file
+    string outputFile = Combine(CurrentDirectory, "output.txt");
+    StreamWriter text = File.CreateText(outputFile);
 
     for (int x = 0; x < layout.GetLength(0); x++)
     {
       for (int y = 0; y < layout.GetLength(1); y++)
       {
+        // output what happening to console
         Console.Write(layout[x, y] + " ");
+
+        // write each line into the output file
+        text.Write(layout[x, y]);
+
+        // with spaces between each character
+        text.Write(" ");
       }
+
+      // add new lines
       Console.WriteLine();
+      text.WriteLine();
     }
+
+    // close access to output file
+    text.Close();
   }
 }
